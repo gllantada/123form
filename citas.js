a0058e56e00a97e7599cc9b13fd463b7d2cc47d0
gllantada <31113423+gllantada@users.noreply.github.com>
2019-09-15 01:29:57 -0300

    gggggggggg
    asdasd asf ggg

--- citas.js
+++ citas.js
@@ -1,5 +1,6 @@
 setTimeout(function(){
        var datos="apiKey=2213741-1568310058-egmat4jwcp25uyhd";
+       console.log("hola1");
        var pageNrArray=Array(0,1,2,3);
        var prospects=0;
        var formularios=$.ajax({
@@ -8,8 +9,10 @@ setTimeout(function(){
          success: function(data){
              prospects=data.submissions;
                 console.log(prospects);
+                console.log("hola2");
              // console.log(formulario.submissions);
              $("div[data-id='56690128']>div[data-role=input-row]>input").blur(function(){
+                    console.log("hola3");
              var id=$(this)[0].value;
              for (var i = 0; i < prospects.length; i++) {
                if(prospects[i].refid==id){
