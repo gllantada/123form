742b20f60a955ff1e74bae5cde23525159df8f66
gonllan <gonzalo.llantada@gmail.com>
2019-09-16 18:26:21 -0300

    test

--- citas.js
+++ citas.js
@@ -2,15 +2,15 @@
 
 window.addEventListener ('load', function () {
 
-var sourceControlId = "56690128", / ** LA ID DEL CONTROL DEL ENVIADOR * /
+var sourceControlId = 56690128, / ** LA ID DEL CONTROL DEL ENVIADOR * /
 
-targetControlId = "56689832", / ** LA ID DEL CONTROL DEL RECEPTOR ** /
+targetControlId = 56689832, / ** LA ID DEL CONTROL DEL RECEPTOR ** /
 
 sourceControlInstance = loader.getEngine (). getDocument (). getElementById (sourceControlId),
 
 targetControlInstance = loader.getEngine (). getDocument (). getElementById (targetControlId);
 
-sourceControlInstance.on ('value-blur', function () {
+sourceControlInstance.on ('blur', function () {
 
 targetControlInstance.setValue (sourceControlInstance.getValue ());
 
