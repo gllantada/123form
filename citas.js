d06e5f158ecc13f935abc01943a67d74e9dec5fc
gonllan <gonzalo.llantada@gmail.com>
2019-09-16 18:21:00 -0300

    tres

--- citas.js
+++ citas.js
@@ -2,9 +2,9 @@
 
 window.addEventListener ('load', function () {
 
-var sourceControlId = 56690128, / ** LA ID DEL CONTROL DEL ENVIADOR * /
+var sourceControlId = "56690128", / ** LA ID DEL CONTROL DEL ENVIADOR * /
 
-targetControlId = 56689832, / ** LA ID DEL CONTROL DEL RECEPTOR ** /
+targetControlId = "56689832", / ** LA ID DEL CONTROL DEL RECEPTOR ** /
 
 sourceControlInstance = loader.getEngine (). getDocument (). getElementById (sourceControlId),
 
