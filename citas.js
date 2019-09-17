fda64a37b93646724835b248e21020279bc01f0c
gonllan <gonzalo.llantada@gmail.com>
2019-09-17 00:58:27 -0300

    tres

--- citas.js
+++ citas.js
@@ -1,12 +1,9 @@
-var numeric_validation_form_id = "5018036]";
-var dropdown_textarea_form_id = "56690128]";
-var image_form_id = "";
 
 
 $(document).ready(function (e) {
 
 
-        $('div[data-id="56690128"]>div:last-child').blur(function () {
+        $('div[data-id="56690128"]>div[data.role="input-row"]>input').change(function () {
         alert("gola");
         });
 
