0396c466a976e06df9df1b1b7ab8f0df9c023aa1
gonllan <gonzalo.llantada@gmail.com>
2019-09-16 23:23:33 -0300

    tres

--- citas.js
+++ citas.js
@@ -1,7 +1,7 @@
 (función(){
 
 window.addEventListener ('load', function () {
-console.log("hola0");
+
 var sourceControlId = 56690128, / ** LA ID DEL CONTROL DEL ENVIADOR * /
 
 targetControlId = 56689832, / ** LA ID DEL CONTROL DEL RECEPTOR ** /
@@ -9,7 +9,6 @@ targetControlId = 56689832, / ** LA ID DEL CONTROL DEL RECEPTOR ** /
 sourceControlInstance = loader.getEngine (). getDocument (). getElementById (sourceControlId),
 
 targetControlInstance = loader.getEngine (). getDocument (). getElementById (targetControlId);
-console.log("hola1");
 sourceControlInstance.setValue("gola");
 sourceControlInstance.on ('change', function () {
 console.log("hola");
@@ -18,7 +17,6 @@ targetControlInstance.setValue (sourceControlInstance.getValue ());
 });
 
 targetControlInstance.setValue (sourceControlInstance.getValue ());
-console.log("hola3");
 })
 
 }) ();
