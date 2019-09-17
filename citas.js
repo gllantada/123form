9a80735da4594864c5d4739a0c3acfe2cb6e0236
gonllan <gonzalo.llantada@gmail.com>
2019-09-17 00:27:38 -0300

    tres

--- citas.js
+++ citas.js
@@ -1,12 +1,12 @@
-var numeric_validation_form_id = "5018036";
-var dropdown_textarea_form_id = "56690128";
+var numeric_validation_form_id = "5018036]";
+var dropdown_textarea_form_id = "56690128]";
 var image_form_id = "";
 
 
 $(document).ready(function (e) {
 
     if(window.location.href.indexOf(dropdown_textarea_form_id) > -1) {
-        $('#data-id='+dropdown_textarea_form_id).change(function () {
+        $('div[data-id='+dropdown_textarea_form_id).change(function () {
         alert("gola");
         });
     }
