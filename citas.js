113057f78c97b1c164983a3c590aa451df25f075
gonllan <gonzalo.llantada@gmail.com>
2019-09-16 23:30:33 -0300

    test

--- citas.js
+++ citas.js
@@ -6,9 +6,9 @@ var sourceControlId = "56690128", / ** LA ID DEL CONTROL DEL ENVIADOR * /
 
 targetControlId = 56689832, / ** LA ID DEL CONTROL DEL RECEPTOR ** /
 
-sourceControlInstance = loader.getEngine (). getDocument (). getElementById (sourceControlId),
+sourceControlInstance = loader.getEngine().getDocument ().getElementById(sourceControlId),
 
-targetControlInstance = loader.getEngine (). getDocument (). getElementById (targetControlId);
+targetControlInstance = loader.getEngine().getDocument ().getElementById(targetControlId);
 sourceControlInstance.setValue("gola");
 sourceControlInstance.on ('change', function () {
 alert("gola");
