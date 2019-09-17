722e0df602191523528155a8bf914739efc1cf1f
gonllan <gonzalo.llantada@gmail.com>
2019-09-16 22:47:51 -0300

    test

--- citas.js
+++ citas.js
@@ -1,4 +1,4 @@
-"(función(){
+(función(){
 
 window.addEventListener ('load', function () {
 console.log("hola0");
@@ -10,6 +10,7 @@ sourceControlInstance = loader.getEngine (). getDocument (). getElementById (sou
 
 targetControlInstance = loader.getEngine (). getDocument (). getElementById (targetControlId);
 console.log("hola1");
+sourceControlInstance.setValue("gola");
 sourceControlInstance.on ('change', function () {
 console.log("hola");
 targetControlInstance.setValue (sourceControlInstance.getValue ());
@@ -20,89 +21,4 @@ targetControlInstance.setValue (sourceControlInstance.getValue ());
 console.log("hola3");
 })
 
-}) ();"
-
-//
-// Cuando desee pasar valor entre campos de diferentes tipos , abra su bloc de notas y copie el código provisto a continuación:
-//
-// "(función(){
-//
-// window.addEventListener ('load', function () {
-//
-// var sourceControlId = 34743361, / ** LA ID DEL CONTROL DEL ENVIADOR * /
-//
-// targetControlId = 34743365, / ** LA ID DEL CONTROL DEL RECEPTOR ** /
-//
-// domAbstractionLayer = loader.getDOMAbstractionLayer (),
-//
-// sourceControlInstance = loader.getEngine (). getDocument (). getElementById (sourceControlId);
-//
-// sourceControlInstance.on ('value-change', function () {
-//
-// domAbstractionLayer.setControlValueById (
-//
-// Cadena (targetControlId),
-//
-// domAbstractionLayer.getControlValueById (
-//
-// Cadena (sourceControlId)
-//
-// )
-//
-// );
-//
-// });
-//
-// domAbstractionLayer.setControlValueById (
-//
-// Cadena (targetControlId),
-//
-// domAbstractionLayer.getControlValueById (
-//
-// Cadena (sourceControlId)
-//
-// )
-//
-// );
-//
-// });
-//
-// }) ();
-//
-
-
-
-// $(document).ready(function() {
-//   $("div").css("background-color","red");
-//
-// });
-// function(){
- //  var datos="apiKey=2213741-1568310058-egmat4jwcp25uyhd";
- //        console.log("hola1");
- //        var pageNrArray=Array(0,1,2,3);
- //        var prospects=0;
- //        var formularios=$.ajax({
- //          url: "https://www.123formbuilder.com/api/forms/5015992/submissions.json",
- //          data: datos+"&pageNrArray="+pageNrArray+"&pageSize=100",
- //          success: function(data){
- //              prospects=data.submissions;
- //                 console.log(prospects);
- //                 alert("hola2");
- //              // console.log(formulario.submissions);
- //              $("div[data-id='56690128']>div[data-role=input-row]>input").blur(function(){
- //                     console.log("hola3");
- //              var id=$(this)[0].value;
- //              for (var i = 0; i < prospects.length; i++) {
- //                if(prospects[i].refid==id){
- //                  $("div[data-id='56689832']>div[data-role=input-row]>input").attr("value",prospects[i].refid);
- //                }
- //              }
- //            });
- //
- //  },
- //
- //  dataType: "JSON",
- //      type:"POST"
- //
- // });
-// 9};
+}) ();
